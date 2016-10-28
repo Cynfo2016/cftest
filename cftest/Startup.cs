348a82161953a21bb16e4e174393dc407cb8bc14
Moises <Moises@MOISES>
2016-10-28 10:53:15 -0400

    Add project files.

---
+++ cftest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cftest.Startup))]
+namespace cftest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
